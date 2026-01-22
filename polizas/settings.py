0c26b1885648c3330b6fd3c20088458604e17437
Byron Valdivieso <bmvaldivieso@utpl.edu.ec>
2026-01-22 10:01:12 -0500

    Cambios en settings y documentación

--- polizas/settings.py
+++ polizas/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "polizas",
         "USER": "root",
-        "PASSWORD": "jose2004",
+        "PASSWORD": "123456789",
         "HOST": "localhost",
         "PORT": "3306",
         "OPTIONS": {
